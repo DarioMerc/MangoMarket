e1d67b00e16e749acfa58e499d5a85cf94d9d660
Dario Mercuri <dario.mercuri31@gmail.com>
2022-10-20 20:44:55 -0400

    models and seeder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import colors from "colors";
 
 const connectDB = async () => {
   try {
@@ -7,9 +8,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.magenta.bold);
   } catch (error) {
-    console.log(`ERROR: ${error.message}`);
+    console.log(`ERROR: ${error.message}`.red.underline);
     process.exit(1);
   }
 };
