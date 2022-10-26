cec67ef1aebf48706460d98d6d234e4f265afe4a
Dario Mercuri <dario.mercuri31@gmail.com>
2022-10-25 23:28:04 -0400

    error exception and started redux

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -19,14 +20,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.use((err, req, res, next) => {
-  const error = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-});
+//ERROR HANDLERS
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
