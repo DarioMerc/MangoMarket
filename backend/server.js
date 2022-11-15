bb5bce67d4b954bff2c55c8e7fa67d2d54d8a8e9
Dario Mercuri <dario.mercuri31@gmail.com>
2022-11-15 17:30:37 -0500

    heroku prep

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
@@ -17,13 +18,26 @@ const app = express();
 app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+
+//DEPLOYMENT
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running!");
+  });
+}
 
 //ERROR HANDLERS
 app.use(notFound);
